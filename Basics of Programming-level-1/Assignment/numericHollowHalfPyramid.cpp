cd6e193a442e0f77c9f660218226b7a117d55396
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-05-21 13:46:47 +0530

    Create numericHollowHalfPyramid.cpp

---
+++ Basics of Programming-level-1/Assignment/numericHollowHalfPyramid.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //numeric half pyramid
+    int n;
+    cout<<"enter the value of n:";
+    cin>>n;
+    //outer loop
+    for(int row=0;row<n;row++){
+        //inner loop
+        for(int col=0;col<row+1;col++){
+            if( col ==0 || row == 0 || row == n-1 || row == col){
+                cout<< col+1 <<" ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
