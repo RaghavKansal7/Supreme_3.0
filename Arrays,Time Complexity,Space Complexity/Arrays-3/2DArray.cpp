66c9600901b38c671e5cbc9b1cea4da6b6b84269
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-05-24 22:50:30 +0530

    Create 2DArray.cpp

---
+++ Arrays,Time Complexity,Space Complexity/Arrays-3/2DArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     //declaring 2d array
+    // int arr[4][3]
+     
+     //initialize
+     int arr[3][2] = {
+                        {10,20},
+                        {30,40},
+                        {50,60}
+                    };
+
+
+
+    return 0;
+}
