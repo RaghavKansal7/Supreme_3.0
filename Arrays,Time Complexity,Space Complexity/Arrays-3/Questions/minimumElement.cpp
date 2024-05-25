6b67a612510ba6a259150f1a8db61c38e076d641
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-05-25 08:50:42 +0530

    Create minimumElement.cpp

---
+++ Arrays,Time Complexity,Space Complexity/Arrays-3/Questions/minimumElement.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int findMinimumIn2DArray(int arr[0][4],int rowSize,int colSize){
+    int minValue = INT_MAX;
+
+    for(int i =0;i<rowSize;i++){
+        for(int j = 0;j<colSize;j++){
+            minValue = min(minValue,arr[i][j]);
+        }
+    }
+    return minValue;
+
+}
+
+int main(){
+    int arr[3][4]={
+        {10,11,12,13},
+        {20,21,22,23},
+        {31,32,44,55}
+    };
+    int rowSize=3;
+    int colSize=4;
+    int ans =findMinimumIn2DArray(arr, rowSize, colSize);
+    cout<< ans << endl;
+    return 0;
+} 
