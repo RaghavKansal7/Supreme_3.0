6a52cb8e54f692796dc54911438f6cf2eb4a1730
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-05-27 23:17:01 +0530

    Create DuplicateNumber.cpp

---
+++ Arrays,Time Complexity,Space Complexity/Assignment/DuplicateNumber.cpp
@@ -0,0 +1,22 @@
+    int findDuplicate(vector<int>& nums) {
+        int len = nums.size();
+        int low = 1;
+        int high = len - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int cnt = 0;
+            for (int i = 0; i < len; i++) {
+                if (nums[i] <= mid) {
+                    cnt++;
+                }
+            }
+
+            if (cnt <= mid) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
