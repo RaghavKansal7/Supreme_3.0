108a6270d68a28f8d20083334e49a7ce63bfdf7d
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-06-02 12:29:46 +0530

    Update InsertionSort.cpp

--- Sorting Algorithms/Insertion Sort/InsertionSort.cpp
+++ Sorting Algorithms/Insertion Sort/InsertionSort.cpp
@@ -1,30 +1,35 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
-//insertion sort
-void print(vector<int> &v){
-    for(int i=0; i<v.size(); i++){
-        cout<<v[i]<<" ";
+
+void insertion_sort(int arr[], int n) {
+    for (int i = 0; i <= n - 1; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
     }
-    cout<<endl;
+
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
 }
-void insertionSort(vector<int> &v) {
-    int n = v.size();
-    // skip i = 0
-    for (int i = 1; i < n; ++i) {
-    int key = v[i];
-    int j = i - 1;
-    // Move elements of arr[0,i-1] that are greater than key to one position ahead of their current position
-    while (j >= 0 && v[j] > key) {
-        v[j + 1] = v[j];
-        j--;
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using insertion Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    v[j + 1] = key; // insertion
-    }
-}
-int main(){
-    vector<int> v={44, 33, 55, 22, 11};
-    insertionSort(v);
-    print(v);
+    cout << endl;
+
+    insertion_sort(arr, n);
     return 0;
 }
