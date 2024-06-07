944e2b3149f961544912f34ce0b3982fe9e214a0
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-06-07 19:35:35 +0530

    Create isomorphicStrings.cpp

---
+++ Char Arrays And Strings/Assignment/isomorphicStrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> mp1;
+        unordered_map<char,char> mp2;
+        int m = s.length();
+        for(int i =0;i<m;i++){
+            char ch1 = s[i];
+            char ch2 = t[i];
+
+            if(mp1.find(ch1) != mp1.end() && mp1[ch1] != ch2
+            || mp2.find(ch2) != mp2.end() && mp2[ch2] != ch1){
+                return false;
+            }
+            mp1[ch1] = ch2;
+            mp2[ch2] = ch1;
+        }
+        return true;
+    }
+};
