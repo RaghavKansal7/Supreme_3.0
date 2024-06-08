4ab16d438dfdc3a4980e1effc218e429891069ae
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-06-08 10:16:49 +0530

    Update longestCommonPrefix.cpp

--- Char Arrays And Strings/Assignment/longestCommonPrefix.cpp
+++ Char Arrays And Strings/Assignment/longestCommonPrefix.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& v) {
-        string ans="";
-        sort(v.begin(),v.end());
-        int n=v.size();
-        string first=v[0],last=v[n-1];
-        for(int i=0;i<min(first.size(),last.size());i++){
-            if(first[i]!=last[i]){
-                return ans;
+    string longestCommonPrefix(vector<string>& strs) {
+        string res="";
+        for(int i = 0;i < strs[0].size();i++){
+            for(auto s:strs){
+                if (s[i] != strs[0][i]){
+                    return res;
+                }
             }
-            ans+=first[i];
+            res += strs[0][i];
         }
-        return ans;
+        return res;
     }
 };
