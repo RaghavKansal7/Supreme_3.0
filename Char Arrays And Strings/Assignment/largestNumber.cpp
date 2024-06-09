6723a85056659df24fda79097e32baed6cd5e86d
Raghav <90691016+RaghavKansal7@users.noreply.github.com>
2024-06-09 14:39:34 +0530

    Create largestNumber.cpp

---
+++ Char Arrays And Strings/Assignment/largestNumber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    static bool mycomp(string a,string b){
+        string t1 = a+b;
+        string t2 = b+a;
+
+        return t1 > t2;
+    }
+
+    string largestNumber(vector<int>& nums) {
+        vector<string>snums;
+        for(auto n:nums){
+            snums.push_back(to_string(n));
+        }
+        sort(snums.begin(),snums.end(),mycomp);
+        if(snums[0] == "0") return "0"; // most important line
+        string ans="";
+        for(auto str:snums){
+            ans += str;
+        }
+        return ans;
+    }
+};
